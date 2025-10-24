96d93a8e2ef4fd83fb6986d6bea3a1c1526e567b
brian (wontae.kim) <u1326@hotmail.com>
2025-10-24 23:22:05 +0900

    fix: build errors 2

--- dapp/app/(mobile)/problem/[id]/page.tsx
+++ dapp/app/(mobile)/problem/[id]/page.tsx
@@ -4,6 +4,16 @@ interface ProblemDetailPageProps {
   params: Promise<{ id: string }>;
 }
 
+// Required for static export with dynamic routes
+export async function generateStaticParams() {
+  // For static export, we need to provide at least one path
+  // Since this is a dynamic bounty app, we'll provide a placeholder
+  // All other routes will be handled client-side
+  return [
+    { id: '1' }
+  ];
+}
+
 export default async function ProblemDetailPage({
   params,
 }: ProblemDetailPageProps) {
